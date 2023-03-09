9cca5554c97e0d8052f66f618122acea556c841e
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-09 23:37:22 +0600

    реализован поиск

--- source/issue_tracker/views/tasks.py
+++ source/issue_tracker/views/tasks.py
@@ -1,17 +1,50 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from django.utils.http import urlencode
 
 from issue_tracker.forms.form_task import TaskForm
 from issue_tracker.models import Task
 
+from issue_tracker.forms.search_forms import SearchForm
 
-class IndexView(TemplateView):
+
+class IndexView(ListView):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.filter(is_deleted=False)
+    model = Task
+    context_object_name = 'tasks'
+    ordering = ('created_at',)
+    paginate_by = 10
+    paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
+
+    def get_queryset(self):
+        queryset = super().get_queryset().exclude(is_deleted=True)
+        if self.search_value:
+            query = Q(summary__icontains=self.search_value) | Q(project__name__icontains=self.search_value) | \
+                    Q(status__name__icontains=self.search_value) | Q(type__name__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
         return context
 
 
